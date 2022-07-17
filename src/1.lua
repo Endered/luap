5bc168b4b9eef63e1e2abfb287c54e0e42892185
Endered <yy56ga10ve@gmail.com>
2022-07-17 23:58:15 +0900

    fix bug "Please remind (print)(123)(print) cause runtime error"

--- src/1.lua
+++ src/1.lua
@@ -1,15 +1,14 @@
 local LUA_TRANSPILE_OBJECTS_ROOT_0 = {}
-LUA_TRANSPILE_OBJECTS_ROOT_0.factorial = function(n)
-local LUA_TRANSPILE_OBJECTS_ROOT_9 = {}
-return (function() if (n <= (1)) then 
-return (1)
-else
-return (n * (LUA_TRANSPILE_OBJECTS_ROOT_0.factorial)((n - (1)))
-)
-end
-end)()
+return (function()
+local LUA_TRANSPILE_OBJECTS_ROOT_27 = {}
+(print)((123))
 
-end
-return (print)((LUA_TRANSPILE_OBJECTS_ROOT_0.factorial)((10))
-)
+_=nil
+(print)((234))
+
+_=nil
+return (print)((345))
+
+
+end)()
 
